de82fc226b117dc52a53013951709cc50c56cd17
Raghu V R <raghuramesh778@gmail.com>
2026-03-24 12:00:26 +0530

    ok

--- billing_project/settings.py
+++ billing_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=9+6$5a)v_p!mmd9m3%b4dl2z-92m(10591h*64y+*qh=^8y%$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
